e1545d031258c84428f74627068ee18df4f44b34
Eugene Rwagasore <rwagasore@gmail.com>
2019-05-02 16:24:47 +0200

    Removed whitespaces

--- rust-fizzbuzz/src/main.rs
+++ rust-fizzbuzz/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     for number in 1..101 {
         let x;
-        
+
         let fzb = if number % 3 == 0 && number & 5 == 0 {
             "FizzBuzz"
         } else if number % 3 == 0 {
@@ -12,7 +12,7 @@ fn main() {
             x = number.to_string();
             &*x
         };
-        
+
         println!("{}", fzb);
     };
 }
